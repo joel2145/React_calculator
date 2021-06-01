6a62d618e562db129d4537c21935f8159f64e131
KikuchiYuta <kikkun0513@gmail.com>
2021-06-01 22:27:41 +0900

    電卓アプリにgithubを導入

---
+++ src/components/Result.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Result = ({result}) => {
+    return (
+        <div className="resultValue">
+            {result}
+        </div>
+    );
+};
---
+++ src/redux/actions/types.js
@@ -0,0 +1,11 @@
+// 数字ボタンが押されたとき
+export const INPUT_NUMBER = "INPUT_NUMBER";
+
+// 記号ボタンが押されたとき
+export const PLUS = "PLUS";
+export const MINUS = "MINUS";
+export const MULTIPLY = "MULTIPLY";
+export const DIVIDE = "DIVIDE";
+
+export const EQUAL = "EQUAL";
+export const CLEAR = "CLEAR";
