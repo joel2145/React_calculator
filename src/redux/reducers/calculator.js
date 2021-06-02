ae5a1306898d7205917a173b41afc3bf064c9052
KikuchiYuta <kikkun0513@gmail.com>
2021-06-02 19:18:12 +0900

    #1_インデントをtab4=>tab2に変更しました。

--- src/redux/reducers/calculator.js
+++ src/redux/reducers/calculator.js
@@ -1,17 +1,17 @@
 import {
-    INPUT_NUMBER,
-    PLUS,
-    MINUS,
-    MULTIPLY,
-    DIVIDE,
-    EQUAL,
-    CLEAR,
+  INPUT_NUMBER,
+  PLUS,
+  MINUS,
+  MULTIPLY,
+  DIVIDE,
+  EQUAL,
+  CLEAR,
 } from "../actions/types";
 
 const initialAppState={
-    inputValue: 0,
-    operator: "",
-    resultValue: 0,
-    calculate: false,
-    showingResult:false,
+  inputValue: 0,
+  operator: "",
+  resultValue: 0,
+  calculate: false,
+  showingResult: false,
 }
