a98c78472853543ec3ba58db50474435e45a0963
KikuchiYuta <kikkun0513@gmail.com>
2021-06-22 15:43:08 +0900

    #1_認証機能開発の開始。TextInputの作成

---
+++ src/firebase/config.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+export const firebaseConfig = {
+  apiKey: "AIzaSyCiMaJeHyd3SKfea4UwoYzAhxMOD4lkZME",
+  authDomain: "react-calculator-6e9b9.firebaseapp.com",
+  projectId: "react-calculator-6e9b9",
+  storageBucket: "react-calculator-6e9b9.appspot.com",
+  messagingSenderId: "877821826563",
+  appId: "1:877821826563:web:00027e4c218540820ba036",
+  measurementId: "G-ZFZHQQ3N8P"
+};
