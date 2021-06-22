2411e6c66a95c3933892970386029408b2bc3214
KikuchiYuta <kikkun0513@gmail.com>
2021-06-22 22:15:16 +0900

    #1_google認証機能の導入

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import { Provider } from 'react-redux';
 
 import App from './App';
 import { reducer } from './redux/reducers';
+import { AuthApp } from './AuthApp';
 
 const Store = createStore(reducer);
 
 ReactDOM.render(
     <Provider store={Store}>
         <App />
+        <AuthApp/>
     </Provider>,
     document.getElementById('root')
 );
